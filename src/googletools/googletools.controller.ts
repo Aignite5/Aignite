e7dba6267992b14c66df55461791cb351ba6888e
monumentale <monumentaleworks@gmail.com>
2025-05-17 12:02:36 +0100

    first commit

--- src/googletools/googletools.controller.ts
+++ src/googletools/googletools.controller.ts
@@ -8,23 +8,23 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @Controller('googletools')
 export class GoogletoolsController {
   constructor(private readonly googletoolsService: GoogletoolsService) {}
-  @Post('create-meeting')
-  @ApiOperation({ summary: 'Create Google Meet event with attendees' })
-  @ApiResponse({
-    status: 201,
-    description: 'Meeting created successfully',
-    schema: {
-      example: {
-        link: 'https://calendar.google.com/event?eid=abc123',
-        meetLink: 'https://meet.google.com/xyz-defg-hij',
-        eventId: 'abc123',
-      },
-    },
-  })
-  @ApiResponse({ status: 500, description: 'Google API error or server issue' })
-  async createGoogleMeetEvent(
-    @Body() dto: CreateGoogleMeetDto,
-  ): Promise<any> {
-    return this.googletoolsService.createEventWithGoogleMeet(dto);
-  }
+  // @Post('create-meeting')
+  // @ApiOperation({ summary: 'Create Google Meet event with attendees' })
+  // @ApiResponse({
+  //   status: 201,
+  //   description: 'Meeting created successfully',
+  //   schema: {
+  //     example: {
+  //       link: 'https://calendar.google.com/event?eid=abc123',
+  //       meetLink: 'https://meet.google.com/xyz-defg-hij',
+  //       eventId: 'abc123',
+  //     },
+  //   },
+  // })
+  // @ApiResponse({ status: 500, description: 'Google API error or server issue' })
+  // async createGoogleMeetEvent(
+  //   @Body() dto: CreateGoogleMeetDto,
+  // ): Promise<any> {
+  //   return this.googletoolsService.createEventWithGoogleMeet(dto);
+  // }
 }
--- src/googletools/googletools.service.ts
+++ src/googletools/googletools.service.ts
@@ -8,75 +8,75 @@ import * as fs from 'fs';
 export class GoogletoolsService {
   constructor() {}
 
-async createEventWithGoogleMeet({
-  summary,
-  description,
-  startDateTime,
-  endDateTime,
-  calendarId = 'primary',
-}: {
-  summary: string;
-  description: string;
-  startDateTime: string;
-  endDateTime: string;
-  calendarId?: string;
-}) {
-  try {
-    // Load service account credentials
-    const credentials = JSON.parse(
-      fs.readFileSync(
-        path.resolve(
-          __dirname,
-          '../../config/authentication-411609-dcd87bcd1c0b.json',
-        ),
-        'utf8',
-      ),
-    );
+// async createEventWithGoogleMeet({
+//   summary,
+//   description,
+//   startDateTime,
+//   endDateTime,
+//   calendarId = 'primary',
+// }: {
+//   summary: string;
+//   description: string;
+//   startDateTime: string;
+//   endDateTime: string;
+//   calendarId?: string;
+// }) {
+//   try {
+//     // Load service account credentials
+//     const credentials = JSON.parse(
+//       fs.readFileSync(
+//         path.resolve(
+//           __dirname,
+//           '../../config/authentication-411609-dcd87bcd1c0b.json',
+//         ),
+//         'utf8',
+//       ),
+//     );
 
-    // Authenticate with Google
-    const auth = new google.auth.GoogleAuth({
-      credentials,
-      scopes: ['https://www.googleapis.com/auth/calendar'],
-    });
+//     // Authenticate with Google
+//     const auth = new google.auth.GoogleAuth({
+//       credentials,
+//       scopes: ['https://www.googleapis.com/auth/calendar'],
+//     });
 
-    const authClient = await auth.getClient();
-    const calendar = google.calendar({ version: 'v3', auth });
+//     const authClient = await auth.getClient();
+//     const calendar = google.calendar({ version: 'v3', auth });
 
-    const response = await calendar.events.insert({
-      calendarId,
-      requestBody: {
-        summary,
-        description,
-        start: {
-          dateTime: startDateTime,
-          timeZone: 'Africa/Lagos',
-        },
-        end: {
-          dateTime: endDateTime,
-          timeZone: 'Africa/Lagos',
-        },
-        // conferenceData: {
-        //   createRequest: {
-        //     requestId: Math.random().toString(36).substring(2),
-        //     conferenceSolutionKey: {
-        //       type: 'hangoutsMeet',
-        //     },
-        //   },
-        // },
-      },
-      conferenceDataVersion: 1,
-    });
+//     const response = await calendar.events.insert({
+//       calendarId,
+//       requestBody: {
+//         summary,
+//         description,
+//         start: {
+//           dateTime: startDateTime,
+//           timeZone: 'Africa/Lagos',
+//         },
+//         end: {
+//           dateTime: endDateTime,
+//           timeZone: 'Africa/Lagos',
+//         },
+//         // conferenceData: {
+//         //   createRequest: {
+//         //     requestId: Math.random().toString(36).substring(2),
+//         //     conferenceSolutionKey: {
+//         //       type: 'hangoutsMeet',
+//         //     },
+//         //   },
+//         // },
+//       },
+//       conferenceDataVersion: 1,
+//     });
 
-    return {
-      meetLink:
-        response.data.conferenceData?.entryPoints?.find(
-          (ep) => ep.entryPointType === 'video',
-        )?.uri || null,
-      calendarEventLink: response.data.htmlLink,
-    };
-  } catch (error) {
-    console.error('Google Calendar Error:', error);
-    throw error;
-  }
-}
+//     return {
+//       meetLink:
+//         response.data.conferenceData?.entryPoints?.find(
+//           (ep) => ep.entryPointType === 'video',
+//         )?.uri || null,
+//       calendarEventLink: response.data.htmlLink,
+//     };
+//   } catch (error) {
+//     console.error('Google Calendar Error:', error);
+//     throw error;
+//   }
+// }
 }
